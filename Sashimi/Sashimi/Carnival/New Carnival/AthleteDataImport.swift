54ae3dea2847ebec5e3b1794c4a31a906d00947c
tomk9126 <tomkeir920@icloud.com>
2024-05-07 20:21:50 +1000

    Added Placeholder Menu Commands

--- Sashimi/Sashimi/Carnival/New Carnival/AthleteDataImport.swift
+++ Sashimi/Sashimi/Carnival/New Carnival/AthleteDataImport.swift
@@ -206,6 +206,7 @@ struct AthleteDataImport: View {
                         }
                     )
                     
+                    
                 }
         
     }
--- Sashimi/SashimiApp.swift
+++ Sashimi/SashimiApp.swift
@@ -12,6 +12,36 @@ struct Swim_CarnivalApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.commands {
+            CommandGroup(after: .newItem) {
+                Menu("New") {
+                    Button("Carnival") { }.disabled(true)
+                    Divider()
+                    Button("Event") { }.disabled(true)
+                    Button("Athlete") { }.disabled(true)
+                }
+                Button("Open Carnival") { }.disabled(true)
+                
+            }
+            CommandMenu("Carnival") {
+                Button("Close Carnival without Saving") { }.disabled(true)
+                Button("Close Carnival and Save Changes") { }.disabled(true)
+                Divider()
+                Button("Edit Carnival") { }.disabled(true)
+                Button("Delete Carnival") { }.disabled(true)
+                Divider()
+                Button("Import Athletes") { }.disabled(true)
+            }
+            CommandMenu("Event") {
+                Button("Score Event") { }.disabled(true)
+                Divider()
+                Button("Edit Event") { }.disabled(true)
+                Button("Delete Event") { }.disabled(true)
+            }
+            CommandMenu("Athlete") {
+                Button("Edit Athlete") { }.disabled(true)
+                Button("Delete Athlete") { }.disabled(true)
+            }
         }
     }
 }
